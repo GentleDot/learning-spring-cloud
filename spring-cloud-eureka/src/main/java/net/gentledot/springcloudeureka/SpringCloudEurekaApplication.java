8886cc255a483aeb6a0380532e98a57b390f5bd6
gentledot <gentledot.wp@gmail.com>
2022-03-09 20:52:57 +0900

    Eureka - feature : @EnableEurekaServer 설정

--- spring-cloud-eureka/src/main/java/net/gentledot/springcloudeureka/SpringCloudEurekaApplication.java
+++ spring-cloud-eureka/src/main/java/net/gentledot/springcloudeureka/SpringCloudEurekaApplication.java
@@ -2,12 +2,14 @@ package net.gentledot.springcloudeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class SpringCloudEurekaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudEurekaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudEurekaApplication.class, args);
+    }
 
 }
