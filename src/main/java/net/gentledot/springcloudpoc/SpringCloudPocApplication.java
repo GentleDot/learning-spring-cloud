63358e6b8cbe4abe45a90929a86529ea0ebac86a
gentledot <gentledot.wp@gmail.com>
2022-03-09 16:27:47 +0900

    first commit

---
+++ src/main/java/net/gentledot/springcloudpoc/SpringCloudPocApplication.java
@@ -0,0 +1,13 @@
+package net.gentledot.springcloudpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/gentledot/springcloudpoc/SpringCloudPocApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gentledot.springcloudpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
