d6953f56ca2166f328ee3fb7c6cef8c4fb43ac68
gentledot <gentledot.wp@gmail.com>
2022-03-09 17:14:04 +0900

    feature : UriConfiguration 설정 (@ConfigurationProperties)

--- src/main/java/net/gentledot/springcloudpoc/gateway/config/RouteConfiguration.java
+++ src/main/java/net/gentledot/springcloudpoc/gateway/config/RouteConfiguration.java
@@ -1,28 +1,32 @@
 package net.gentledot.springcloudpoc.gateway.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UriConfiguration.class)
 public class RouteConfiguration {
 
     @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator myRoutes(RouteLocatorBuilder builder, UriConfiguration uriConfiguration) {
+        String httpUri = uriConfiguration.getHttpbin();
+
         return builder.routes()
             .route(predicateSpec ->
                 predicateSpec.path("/get")
                     .filters(
                         gatewayFilterSpec -> gatewayFilterSpec.addRequestHeader("Hello", "World"))
-                    .uri("http://httpbin.org:80"))
+                    .uri(httpUri))
             .route(predicateSpec ->
                 predicateSpec.host("*.circuitbreaker.com")
                     .filters(gatewayFilterSpec -> gatewayFilterSpec
                         .circuitBreaker(config ->
                             config.setName("mycmd")
                                 .setFallbackUri("forward:/fallback")))
-                    .uri("http://httpbin.org:80"))
+                    .uri(httpUri))
             .build();
     }
 }
---
+++ src/main/java/net/gentledot/springcloudpoc/gateway/config/UriConfiguration.java
@@ -0,0 +1,16 @@
+package net.gentledot.springcloudpoc.gateway.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "uri")
+public class UriConfiguration {
+    private String httpbin = "http://httpbin.org:80";
+
+    public String getHttpbin() {
+        return httpbin;
+    }
+
+    public void setHttpbin(String httpbin) {
+        this.httpbin = httpbin;
+    }
+}
